59442aec204647ee547a23e1241a6d335836ba63
raha <91209882+b1tcrushr@users.noreply.github.com>
2026-01-27 17:10:26 -0500

    Create Note class with attributes and methods
    Implement Note class with properties and methods for overlap checking.

---
+++ server/note.java
@@ -0,0 +1,68 @@
+"""
+-------------------------------------------------------
+Note Data Structure - (x, y, height, width, colour, message)
+-------------------------------------------------------
+Author: Rahnuma Haque
+ID: 169024593
+Email: haqu4593@mylaurier.ca
+__updated__ = "2026-01-27"
+-------------------------------------------------------
+"""
+public class Note {
+  private final int x, y; 
+  private final int height, width;
+  private final String colour;
+  private final String message;
+
+  public Note(int x, int y, int height, int width, String colour, String message) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.colour = colour;
+    this.message = message;
+  }
+
+  public int getX() {
+    return x; 
+  }
+
+  public int getY() {
+    return y; 
+  }
+
+  public int getHeight() {
+    return height; 
+  }
+
+  public int getWidth() {
+    return width; 
+  }
+
+  public int getColour() {
+    return colour; 
+  }
+
+  public int getMsg() {
+    return message; 
+  }
+  /* -----------------------------------------------------
+  check if note COMPLETELY overlaps with another note, n 
+  ('Completely overlaps' - occupy the exact same rectangular region)
+  ----------------------------------------------------- */
+  public boolean completelyOverlaps(Note n) {
+    return this.x == n.x && this.y == n.y && this.height == n.height && this.width == n.width;
+  }
+
+  /* -----------------------------------------------------
+  check if note PARTIALLY overlaps with another note, n 
+  ----------------------------------------------------- */
+  
+  // confirm if required ?
+
+  @Override // native java function, explicity use defined function
+  public String toString() {
+    return String.format("Note (x = %d, y = %d, colour = %s, message = %s)", x, y, colour, message);
+  }
+  
+}
