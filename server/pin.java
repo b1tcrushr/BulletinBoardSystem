11a84ebebd456bbcaa51954ab8a7dd3d74b2413d
raha <91209882+b1tcrushr@users.noreply.github.com>
2026-01-27 17:28:08 -0500

    Add Pin class
    Implement Pin class to represent coordinates.

---
+++ server/pin.java
@@ -0,0 +1,32 @@
+"""
+-------------------------------------------------------
+Pin Data Structure - (x, y)
+-------------------------------------------------------
+Author: Rahnuma Haque
+ID: 169024593
+Email: haqu4593@mylaurier.ca
+__updated__ = "2026-01-27"
+-------------------------------------------------------
+"""
+
+public class Pin {
+  private final int x, y;
+
+  public Pin(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+// confirm pin interaction when placed on same coordinate  
+  
+  }
+  
+}
